c894914ebf0216cc2f5b2b5f05373eab670cf3a0
ALPS9999 <aalps88@hotmail.com>
2026-06-02 11:54:41 +0800

    PWA support: manifest + service worker + iOS meta tags

---
+++ sw.js
@@ -0,0 +1,13 @@
+// 识字小达人 - Service Worker
+const CACHE = 'shizi-v1';
+const FILES = ['/projects/', '/projects/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
